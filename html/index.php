ded83d5cbfdee8ddaba4c9ea88a11623bd90eb0b
Jeff Minard <jeff@jrm.cc>
2016-09-07 21:48:08 -0700

    Add allocations, evals, and make things link around the page nicely

--- html/index.php
+++ html/index.php
@@ -1,4 +1,9 @@
-<!doctype html>
+<?php
+
+// this works for me. Sorry!
+date_default_timezone_set('America/Los_Angeles');
+
+?><!doctype html>
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
@@ -15,6 +20,10 @@
         <script src="bootstrap.min.js"></script>
 
         <style>
+            * {
+                font-family: monospace;
+                font-size: 14px;
+            }
             td.moreInfo {
                 display: none;
             }
@@ -22,6 +31,17 @@
             td ul{
                 padding: 0;
                 padding-left: 10px;
+            }
+            tr:target {
+                background: rgba(16, 255, 0, 0.15);
+            }
+            .table > thead > tr > th,
+            .table > tbody > tr > th,
+            .table > tfoot > tr > th,
+            .table > thead > tr > td,
+            .table > tbody > tr > td,
+            .table > tfoot > tr > td {
+                padding: 2px 8px;
             }
         </style>
 
@@ -42,9 +62,15 @@ $nomadBaseUrl = getenv('NOMAD_BASEURL') ?: 'http://127.0.0.1:4646';
 
 $nodesRaw = @file_get_contents($nomadBaseUrl . '/v1/nodes');
 $jobsRaw  = @file_get_contents($nomadBaseUrl . '/v1/jobs');
+$allocsRaw= @file_get_contents($nomadBaseUrl . '/v1/allocations');
+$evalsRaw = @file_get_contents($nomadBaseUrl . '/v1/evaluations');
 
 $nodes = @json_decode($nodesRaw);
 $jobs  = @json_decode($jobsRaw);
+$allocs= @json_decode($allocsRaw);
+$evals = @json_decode($evalsRaw);
+
+// echo '<pre>' . print_r($evals,1) . '</pre>';
 
 $nodeInfos = [];
 foreach ((array)$nodes as $node) {
@@ -54,6 +80,10 @@ foreach ((array)$nodes as $node) {
 $jobInfos = [];
 foreach ((array)$jobs as $job) {
     $jobInfos[$job->ID] = json_decode(file_get_contents($nomadBaseUrl . '/v1/job/' . $job->ID));
+}
+
+function interlink($type, $val) {
+    return '<a href="#'.$type.'-'.$val.'">' . explode('-', $val)[0] .'</a>';
 }
 
 ?>
@@ -89,7 +119,7 @@ foreach ((array)$jobs as $job) {
 
                     <?php foreach ($nodeInfos as $nodeId => $nodeInfo) { ?>
 
-                    <tr>
+                    <tr id="node-<?= $nodeInfo->ID ?>">
                         <td><?= $nodeInfo->ID ?></td>
                         <td><?= $nodeInfo->Name ?></td>
                         <td><?= $nodeInfo->HTTPAddr ?></td>
@@ -159,6 +189,86 @@ foreach ((array)$jobs as $job) {
                         </td>
                     </tr>
 
+                    <?php } ?>
+
+                </tbody>
+            </table>
+
+            <h2>Allocations</h2>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Created</th>
+                        <th>JobID</th>
+                        <th>NodeID</th>
+                        <th>Desired</th>
+                        <th>Current</th>
+                        <th>EvalID</th>
+                        <th>Task Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($allocs as $allocation) { ?>
+                    <tr id="alloc-<?= $allocation->ID ?>">
+                        <td nowrap><?= date('Y-m-d H:i:s', $allocation->CreateTime/1000000000) ?></td>
+                        <td><?= $allocation->JobID ?></td>
+                        <td><?= interlink('node', $allocation->NodeID) ?></td>
+                        <td><?= $allocation->DesiredStatus ?></td>
+                        <td><?= $allocation->ClientStatus ?></td>
+                        <td><?= interlink('eval', $allocation->EvalID) ?></td>
+                        <td><button class="moreInfo btn btn-default btn-xs">Task&nbsp;Details</button></td>
+                    </tr>
+                    <tr style="display: none">
+                        <td colspan=99>
+                        <?php foreach ($allocation->TaskStates as $TaskID => $task) { ?>
+                            Task: <b><?= $TaskID ?></b>, State: <b><?= $task->State ?></b></br>
+                            <?php foreach ($task->Events as $event) { ?>
+                            <?= date('Y-m-d H:i:s', $event->Time/1000000000) ?>:
+                            <?= $event->Type ?>
+                            <?= $event->DriverError ?>
+                            <?= $event->RestartReason ?>
+                            <?= $event->Message ?>
+                            <?= $event->KillError ?>
+                            <?= $event->DownloadError ?>
+                            <?= $event->ValidationError ?>
+                            <br/>
+                            <?php } ?>
+                        <?php } ?>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+            <h2>Evaluations</h2>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>JobID</th>
+                        <th>Type</th>
+                        <th>Priority</th>
+                        <th>NodeID</th>
+                        <th>Status</th>
+                        <th>More Info</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($evals as $evalInfo) { ?>
+                    <tr id="eval-<?= $evalInfo->ID ?>">
+                        <td><?= $evalInfo->ID ?></td>
+                        <td><?= $evalInfo->JobID ?></td>
+                        <td><?= $evalInfo->Type ?></td>
+                        <td><?= $evalInfo->Priority ?></td>
+                        <td><?= interlink('node', $evalInfo->NodeID) ?></td>
+                        <td><?= $evalInfo->Status ?></td>
+                        <td><button class="moreInfo btn btn-default btn-xs">Full&nbsp;Info</button></td>
+                    </tr>
+                    <tr style="display: none">
+                        <td colspan=99>
+                            <pre><?= json_encode($evalInfo, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT) ?></pre>
+                        </td>
+                    </tr>
                     <?php } ?>
 
                 </tbody>
