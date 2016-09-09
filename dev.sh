57acd59572a1bf1e056a3223f52a2b68c3def4a8
Jeff Minard <jeff@jrm.cc>
2016-09-08 22:53:30 -0700

    redundant to readme

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# In _my_ environment, consul.service.consul is the same host that run nomad.
-# Customize as you see fit
-docker run -ti --rm \
- -v `pwd`/html:/var/www/html/ \
- -e 'NOMAD_BASEURL=http://consul.service.consul:4646' \
- php:5.6-apache
