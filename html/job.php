cd55781b64e6390d38b0a105df9ecb63ead2a3eb
Jeff Minard <jeff@jrm.cc>
2016-09-11 22:26:59 -0700

    Enhance job page with latest allocations, allocation sorting, and allocation history messages

--- html/job.php
+++ html/job.php
@@ -18,6 +18,53 @@ $jobInfo            = @json_decode($items[0]);
 $jobAllocationsInfo = @json_decode($items[1]);
 $jobEvaluationsInfo = @json_decode($items[2]);
 
+// sort allocations by createtime
+usort($jobAllocationsInfo, function($a, $b) {
+    if ($a->CreateTime == $b->CreateTime) {
+        return 0;
+    }
+    return ($a->CreateTime < $b->CreateTime) ? 1 : -1;
+});
+
+// Find latest allocation(s) info, tie it in
+foreach ($jobInfo->TaskGroups as $tgidx => $taskGroup) {
+
+    $allocationsIds = [];
+    for($i = 0; $i < $taskGroup->Count; $i++) {
+        foreach ($jobAllocationsInfo as $ainfo) {
+            // ex: loopy.loopy-group-two[0]
+            if ($ainfo->Name == $jobInfo->Name . '.' . $taskGroup->Name . '[' . $i . ']') {
+                $allocationsIds[] = $ainfo->ID;
+                break;
+            }
+        }
+    }
+    $jobInfo->TaskGroups[$tgidx]->AllocationIDs = $allocationsIds;
+}
+
+// Generate a lovely list of all allocation events
+$allocationHistory = [];
+foreach ($jobAllocationsInfo as $ainfo) {
+    foreach ($ainfo->TaskStates as $taskId => $taskInfo) {
+        foreach ($taskInfo->Events as $eventInfo) {
+            $allocationHistory[] = array_merge([
+                'ID' => $ainfo->ID,
+                'EvalID' => $ainfo->EvalID,
+                'NodeID' => $ainfo->NodeID,
+                'GroupStatus' => $ainfo->ClientStatus,
+                'Name' => explode('.', $ainfo->Name, 2)[1],
+                'Task' => $taskId,
+            ], (array)$eventInfo);
+        }
+    }
+}
+usort($allocationHistory, function($a, $b) {
+    if ($a['Time'] == $b['Time']) {
+        return 0;
+    }
+    return ($a['Time'] < $b['Time']) ? 1 : -1;
+});
+
 $twig = Nomad\Nomad::getTwig();
 
 echo $twig->render('job.html.twig', array(
@@ -27,4 +74,5 @@ echo $twig->render('job.html.twig', array(
     'job' => $jobInfo,
     'allocations' => $jobAllocationsInfo,
     'evaluations' => $jobEvaluationsInfo,
+    'allocationHistory' => $allocationHistory,
 ));
