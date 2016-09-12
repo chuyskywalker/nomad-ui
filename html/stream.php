b3ee5af258233a2ef15999ae2437d0e853cec119
Jeff Minard <jeff@jrm.cc>
2016-09-11 22:37:31 -0700

    Prevent xss attacks by escape appending and content header setting

--- html/stream.php
+++ html/stream.php
@@ -6,6 +6,8 @@ require 'vendor/autoload.php';
 
 $info = (new Nomad\Crypt())->decrypt($_GET['fileid']);
 
+header('Content-type: text/plain');
+
 echo file_get_contents(
     'http://' . $info['address'] . '/v1/client/fs/readat/' . $info['allocationid']
     .'?path='. $info['path'] . '&offset='. $_GET['offset'] .'&limit=' . $_GET['limit']
