238b9abddd86ea4fabdca008d73d9a09f64aebfd
Jeff Minard <jeff@jrm.cc>
2016-09-16 12:03:59 -0700

    Dont wait forever for Nomad in case of timeouts

--- html/src/Nomad.php
+++ html/src/Nomad.php
@@ -55,6 +55,8 @@ class Nomad {
             curl_setopt($curly[$id], CURLOPT_URL,            $url);
             curl_setopt($curly[$id], CURLOPT_HEADER,         0);
             curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curly[$id], CURLOPT_CONNECTTIMEOUT, 1);
+            curl_setopt($curly[$id], CURLOPT_TIMEOUT,        3);
 
             // post?
             if (is_array($d)) {
